e3d4dfd5af09e413314bb03d62f3232978971aa5
allenyllee <allen7575@gmail.com>
2021-09-04 15:19:58 +0800

    change owner and mode of syslogng-logrotate.conf to avoid `logrotate -df /etc/logrotate.d/syslogng-logrotate.conf` failed

--- syslogng/init.sh
+++ syslogng/init.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 echo "$@"
+/bin/chown root:root /etc/logrotate.d/syslogng-logrotate.conf
+/bin/chmod 644 /etc/logrotate.d/syslogng-logrotate.conf
 /etc/init.d/cron start
 /usr/sbin/syslog-ng "$@"
