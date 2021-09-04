8fb9e1442f6412db3e031023e54221bbe13eb6a9
allenyllee <allen7575@gmail.com>
2021-09-04 15:24:54 +0800

    adding test cmd `/usr/sbin/logrotate -df /etc/logrotate.d/syslogng-logrotate.conf` before start syslog-ng

--- syslogng/init.sh
+++ syslogng/init.sh
@@ -2,5 +2,6 @@
 echo "$@"
 /bin/chown root:root /etc/logrotate.d/syslogng-logrotate.conf
 /bin/chmod 644 /etc/logrotate.d/syslogng-logrotate.conf
+/usr/sbin/logrotate -df /etc/logrotate.d/syslogng-logrotate.conf
 /etc/init.d/cron start
 /usr/sbin/syslog-ng "$@"
