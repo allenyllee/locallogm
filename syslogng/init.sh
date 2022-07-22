76923e2ffbe2536ffb4015be79dacc31d20b6bff
allenyllee <allen7575@gmail.com>
2022-07-22 20:20:58 +0800

    fix permission error: `error: Ignoring /etc/logrotate.d/syslogng-logrotate.conf because it is writable by group or others.` by using copied `syslogng-logrotate.conf`.

--- syslogng/init.sh
+++ syslogng/init.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 echo "$@"
+/bin/cp /syslogng/syslog-ng.conf /etc/syslog-ng/syslog-ng.conf
+/bin/cp /syslogng/syslogng-logrotate.conf /etc/logrotate.d/syslogng-logrotate.conf
 /bin/chown root:root /etc/logrotate.d/syslogng-logrotate.conf
 /bin/chmod 644 /etc/logrotate.d/syslogng-logrotate.conf
-/usr/sbin/logrotate -df /etc/logrotate.d/syslogng-logrotate.conf
+# /usr/sbin/logrotate -df /etc/logrotate.d/syslogng-logrotate.conf # only for debug use
 /etc/init.d/cron start
 /usr/sbin/syslog-ng "$@"
