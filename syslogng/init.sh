895360dc4fe1200004af3bbddd3424a63350d0eb
allenyllee <allen7575@gmail.com>
2023-03-12 18:32:33 +0800

    add 'apt-get update && apt-get ins tall -y cron' before run cron start

--- syslogng/init.sh
+++ syslogng/init.sh
@@ -5,5 +5,6 @@ echo "$@"
 /bin/chown root:root /etc/logrotate.d/syslogng-logrotate.conf
 /bin/chmod 644 /etc/logrotate.d/syslogng-logrotate.conf
 # /usr/sbin/logrotate -df /etc/logrotate.d/syslogng-logrotate.conf # only for debug use
+apt-get update && apt-get install -y cron
 /etc/init.d/cron start
 /usr/sbin/syslog-ng "$@"
